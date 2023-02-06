d8fadbc7024798d27c618c625ab6c3a0b881208d
obuddy <124168007+obuddy@users.noreply.github.com>
2023-02-06 02:12:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = https://obuddy.netlify.app/;
+const port = obuddy.netlify.app;
 
 const configuration = new Configuration({
     organization: "org-dZvNRCYFmarPX8qVRRXiErTD",
